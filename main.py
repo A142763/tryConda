b444644aafacfc6c828241cba37831ada1c18737
Jimmy James <A142763@users.noreply.github.com>
2024-05-28 21:54:49 -0700

    Here we go...

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # This is a sample Python script.
+# Script Kiddies R Us
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -12,5 +13,3 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('try python')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
